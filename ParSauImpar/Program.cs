33703afd3534397fe5ed23a861302ef37211c811
DavidBatkay <david.batkay@emanuel.ro>
2023-10-16 21:01:02 +0300

    tema

---
+++ ParSauImpar/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ParSauImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.Write("Introduceti un numar intreg: ");
+            a = int.Parse(Console.ReadLine());
+            if (a%2==0)
+            Console.WriteLine("Numarul este par");
+            else
+            Console.WriteLine("Numarul este impar");
+        }
+    }
+}
